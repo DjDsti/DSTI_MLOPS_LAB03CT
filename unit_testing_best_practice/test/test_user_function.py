4fdd72c26c786cea6b605bdc2f80f41a79682ac6
DjDsti <djamila.mokhtari@edu.dsti.institute>
2026-03-17 16:25:38 +0100

    Update test_user_function.py

--- unit_testing_best_practice/test/test_user_function.py
+++ unit_testing_best_practice/test/test_user_function.py
@@ -1,9 +1,11 @@
-
-import sys
-sys.path = ['DSTI_MLOPS_LAB03CT/unit_testing_best_practice/src']
-from user_function.py import *
 import pytest
 import io
+from sys import path as syspath
+syspath.append("../src")
+#import sys
+#sys.path = ['DSTI_MLOPS_LAB03CT/unit_testing_best_practice/src']
+from user_function.py import *
+
 def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
     assert get_email_from_input() is None
