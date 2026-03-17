042ecf90cb50112c97b427b2b8052f3481c0b838
DjDsti <djamila.mokhtari@edu.dsti.institute>
2026-03-17 17:21:16 +0100

    Update user_function.py

--- unit_testing_best_practice/src/user_function.py
+++ unit_testing_best_practice/src/user_function.py
@@ -16,6 +16,20 @@
 
 # Do the same for the following functions
 # Functions in src/user_functions.py and tests in tests/test_user_functions.py
+# The simplest function to get the user email (copy to src/user_functions.py)
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    return input("Tell me your email: ")
+
+# More elaborated version (copy to src/user_functions.py)
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Tell me your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
 
 def get_user_name_from_input():
     """ Not empty string. No spaces. """
