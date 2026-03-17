d68c03282c57bcfe73a9fe1d2bcf730a808397a7
DjDsti <djamila.mokhtari@edu.dsti.institute>
2026-03-17 16:35:02 +0100

    Update test_user_function.py

--- unit_testing_best_practice/test/test_user_function.py
+++ unit_testing_best_practice/test/test_user_function.py
@@ -1,8 +1,8 @@
 import pytest
 import io
 from sys import path as syspath
-syspath.append("unit_testing_best_practice/src")
-from unit_testing_best_practice/src/user_function.py import *
+syspath.append("./unit_testing_best_practice/src")
+from user_function.py import *
 
 def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
